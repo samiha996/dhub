bb75f68f0fa69f438ab97393feb869a124e2d71d
youseff-dow-group <youssef@dowgroup.com>
2025-02-05 19:03:02 +0300

    Update sale_order.py

--- custom_crm_deehub_module/models/sale_order.py
+++ custom_crm_deehub_module/models/sale_order.py
@@ -10,14 +10,6 @@ class SaleOrder(models.Model):
         comodel_name='res.partner', related='opportunity_id.agent'
     )
 
-    related_lead_call_ids = fields.One2many(
-        comodel_name="crm.lead.call",
-        inverse_name="lead_id",
-        string="Related Lead Calls",
-        compute="_compute_related_lead_calls",
-        store=True
-    )
-
     lead_calls_count = fields.Integer(
         related='opportunity_id.lead_calls_count'
     )
@@ -25,11 +17,5 @@ class SaleOrder(models.Model):
         comodel_name='res.country',related='opportunity_id.study_country', string='Study Country'
     )
 
-    @api.depends("opportunity_id")
-    def _compute_related_lead_calls(self):
-        for record in self:
-            if record.opportunity_id:
-                record.related_lead_call_ids = record.opportunity_id.lead_call_ids
-            else:
-                record.related_lead_call_ids = False
+
 
